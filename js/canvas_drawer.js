f9974f1738cc70e30372382d47862bef14bfd440
Katherine Hayden <katherineimogene@gmail.com>
2014-09-11 14:52:12 -0700

    saving WIP from Fluxx machine

--- js/canvas_drawer.js
+++ js/canvas_drawer.js
@@ -1,28 +1,37 @@
-Boa.CanvasDrawer = function(width, height){
-         this.canvas = document.createElement('canvas'),
-            this.ctx = this.canvas.getContext('2d');
-   this.canvas.width = width;
-  this.canvas.height = height;
-            this.div = document.getElementById('game-canvas')
-}
+// Boa.CanvasDrawer = function(width, height){
+//          this.canvas = document.createElement('canvas'),
+//             this.ctx = this.canvas.getContext('2d');
+//    this.canvas.width = width;
+//   this.canvas.height = height;
+//             this.div = document.getElementById('game-canvas')
+// }
 
-Boa.CanvasDrawer.prototype = {
-  draw: function(){
-    this.div.appendChild(this.canvas);
-    this.ctx.lineWidth   = 2;       // Our border will have a thickness of 2 pixels
-    this.ctx.strokeStyle = 'black'; // The border will also be black
-    this.ctx.strokeRect(2, 20, this.canvas.width - 4, this.canvas.height - 24);
-  },
+// Boa.CanvasDrawer.prototype = {
+//   draw: function(){
+//     this.div.appendChild(this.canvas);
+//     this.ctx.lineWidth   = 2;       // Our border will have a thickness of 2 pixels
+//     this.ctx.strokeStyle = 'black'; // The border will also be black
+//     this.ctx.strokeRect(2, 20, this.canvas.width - 4, this.canvas.height - 24);
+//   },
 
-  makeMatrix: function(){
-    var gameMatrix = this.gameMatrix
-    gameMatrix = new Array(71)
-    for (var i=0; i<gameMatrix.length; i++){
-      gameMatrix[i] = new Array(33)
-    }
-    console.log(gameMatrix)
-  }
-}
+//   makeMatrix: function(){
+//     var gameMatrix = this.gameMatrix
+//     gameMatrix = new Array(71)
+//     for (var i=0; i<gameMatrix.length; i++){
+//       gameMatrix[i] = new Array(33)
+//     }
+//     console.log(gameMatrix)
+//   }
+// }
+
+
+
+// // $(function() {
+// //   var canvas
+// //   canvas = new Boa.CanvasDrawer(720,336)
+// //   canvas.draw()
+// //   canvas.makeMatrix()
+// // })
 
 
 
--- js/initialization.js
+++ js/initialization.js
@@ -1,9 +1,75 @@
 // namespace
-Boa = {}
+BOA = {}
 
-$(function() {
-  var canvas
-  canvas = new Boa.CanvasDrawer(720,336)
-  canvas.draw()
-  canvas.makeMatrix()
+BOA.Game = function(){
+	this.ctx = null
+	this.xPosition = 0
+	this.yPosition = 0
+	this.frameLength = 300
+	this.canvasSideLength = 700
+}
+
+BOA.Game.prototype = {
+	initialize: function(){
+		this.readyDOM()
+		this.gameLoop()
+	},
+
+	readyDOM: function(){
+		$('body').append('<canvas id="game-canvas">')
+		var $canvas = $('#game-canvas')
+		$canvas.attr('width', this.canvasSideLength)
+		$canvas.attr('height', this.canvasSideLength)
+		var canvas = $canvas[0]
+		this.ctx = canvas.getContext('2d')
+	},
+
+	gameLoop: function(){
+		this.xPosition += 2
+		this.yPosition += 4
+		this.ctx.clearRect(0,0,this.canvasSideLength,this.canvasSideLength)
+		this.ctx.fillStyle = '#fe57a1'
+		this.ctx.fillRect(this.xPosition, this.yPosition, 50, 70)
+		setTimeout(this.gameLoop, this.frameLength)
+	}
+
+}
+
+$(document).ready(function(){
+	var boaGame = new BOA.Game()
+	boaGame.initialize()
 })
+
+// context
+// xPosition
+// yPosition
+// frameLength
+
+// initialization:
+// <canvas> element with an id
+// width and height
+// ctx = canvas.getContext('2d')
+
+
+// // The standard way to make movement in a game is with the game loop. This is a function that gets called at a fixed interval to update the state of the game and display the changes.
+// run the game
+// change xPosition
+// change yPosition
+// clear the canvas clearRect(a,b,c,d)
+// fill the canvas fillStyle hexcode
+// fill the canvas fillRect(a,b,c,d)
+// getTimeout(gameLoop, frameLength)
+
+
+// //snake
+// posArray
+// direction
+
+// fn drawSection
+// draw snake
+// advance snake
+
+// //user: moving the snake
+// bind events keycodes 37,38,39,40 and keydown
+// set direction
+// valid directions
